316b2817015d92bfb499fb511c1a0b042630b90a
pankajsharma <yellow*99>
2020-02-18 10:45:49 +0530

    adding gradlew file

--- src/main/java/com/viku/userplatform/controller/UserController.java
+++ src/main/java/com/viku/userplatform/controller/UserController.java
@@ -19,6 +19,7 @@ public class UserController {
     User registerUser(@RequestBody User user){
         log.info("Request : {}",user);
         User savedUser=userRepository.save(user);
+        log.info("Response : {}",savedUser);
         return savedUser;
     }
     @PostMapping("/health")
@@ -29,7 +30,9 @@ public class UserController {
     @PostMapping("/login")
     User loginUser(@RequestBody Map<String,String> login){
         log.info("Request : {}",login);
-        return userRepository.findUserByEmailAndPassword(
+        User user =userRepository.findUserByEmailAndPassword(
                 login.get("email"),login.get("password"));
+        log.info("Response : {}",user);
+        return user;
     }
 }
